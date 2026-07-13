2f5602f023ffe986fe467c685d35ad6da8c60054
LauraRiquetti <laurariquetti@gmail.com>
2026-07-13 18:01:39 -0300

    fix - correção migration

--- database/migrations/2026_07_08_221721_create_vendedoras_table.php
+++ database/migrations/2026_07_08_221721_create_vendedoras_table.php
@@ -18,6 +18,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->date('data_nascimento');
+            $table->integer('CPF')->unique();
+            $table->string('telefone');
             $table->integer('CEP');
             $table->string('rua');
             $table->string('bairro');
